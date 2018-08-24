13d3fc2d39b8167208a969581e068860a1b7a02c
beattyre <>
2018-08-23 22:40:53 -0400

    Working

--- kexp-get.py
+++ kexp-get.py
@@ -1,91 +1,112 @@
 """
-goal output: csv file containing artist - song name for each of the songs in the latest KEXP Music That Matters
-             podcast
-
+goal output: grab latest KEXP Music that Matters playlist, search spotify for the songs, and add any song that returns a match
 title: kexp-get.py
 author: robert beatty
-date: march 14, 2018
+date: July 2, 2018
 """
 
 from urllib import request as r
 from timeit import default_timer as t
 from bs4 import BeautifulSoup
-from datetime import datetime as dt
+import spotipy
+import os
 import re
-
-time_start = t()
-today = dt.now()
-day, month, year = today.day, today.month, today.year
-today = "{0}/{1}/{2}".format(month,day,year)
+import spotipy.util as util
+t_s = t()
 print("start...\n")
-
 
 # it's a globalized world
 kv_latest = []
 lines = []
 
-time_siteaccess_start = t()
-x = r.urlopen('https://tunein.com/radio/Music-That-Matters-Podcast-p117040/', data=None, timeout=5) # access webpage
-time_siteaccess_end = t()
-time_siteaccess_duration = round((time_siteaccess_end - time_siteaccess_start),2)
-# check server code
-if x.getcode() != 200: 
+x = r.urlopen('http://feeds.kexp.org/kexp/musicthatmatters', data=None, timeout=5) # jawn
+# good jawn?
+if x.getcode() != 200:
 	print("[!] Warning [!]")
 else:
-	print("successfully accessed website") 
-	print("connected to website in {}s\n".format(time_siteaccess_duration))
+	print("successfully accessed website")
 
-# see if there's a new podcast today...
+
+# new jawn?
 try:
-	time_tb1_start = t()
-	kv_latest = []
-	dump = BeautifulSoup(x, 'lxml')
-	if len(dump) >0: 
-		a = dump.find(id="container-0") 
-		b = a.contents[1].strings
-		cnt = 0
-		for str in b:
-			kvp ={'i':cnt,
-				'v':str
-				}
-			kv_latest.append(kvp)
-			cnt += 1
+	t_tb1_s = t()
+	dump = BeautifulSoup(x, 'lxml-xml')
+	if len(dump) >0:
+		a = dump.find_all("description")
+		new = str(a[1].get_text())
 	else:
-		print("[!] Warning -- dump failed\n")
-	date = kv_latest[1]['v']
-	if date == today:
-		print('nyet\n')
-	else:
-		print('yas queen')
-	time_tb1_end = t()
-	time_tb1_duration = round((time_tb1_end - time_tb1_start),2)
-	print("try block one completed in {}s\n".format(time_tb1_duration))
-except:
-	print("[!] Warning -- in try block 1 [!]")
+		print("dump failed")
+except Exception as e:
+	print(e.message)
 
 # TRY BLOCK TWO: let's create a csv(?) containing the songs aired on the latest podcast
 try:
-	time_tb2_start = t()
-	title_seg = re.split(",\s", kv_latest[0]['v'])
-	title_seg = re.split("\s-\s", title_seg[1])
-	title_vol, title_name = title_seg[0], title_seg[1]
-	lines = re.split("\s\d+.\s", kv_latest[3]['v'])
+	lines = re.split("\s\d+.\s", new)
+	cnt = 1
+	song_list = []
+	# make a jawn
+	# open jawn
 	for i in lines:
-		# print(i)
-		if re.search("\w+?\s-\s*?\w+?", i):
-			print(i)
+		# that good jawn
+		m = re.match(r'^(.*?)\s-\s(.*?)$', i)
+		if '<' in i:
+			j = str(i)
+			spt = j.split('<')
+			spt2 = spt[0].split(' - ')
+			song = {
+				'track': cnt,
+				'artist': spt2[0],
+				'name': spt2[1]
+			}
+			song_list.append(song)
+			del song
+			cnt +=1
+		elif m:
+			spt = i.split(' - ')
+			# print(spt[1])
+			song = {
+				'track': cnt,
+				'artist': spt[0],
+				'name': spt[1]
+			}
+			song_list.append(song)
+			del song
+			cnt +=1
+except Exception as e:
+	print(e.message)
 
-	time_tb2_end = t()
-	time_tb2_duration = round((time_tb2_end - time_tb2_start),2)
-	print("try block two completed in {}s\n".format(time_tb2_duration))
 
+## TRY BLOCK 3: Spotify stuff
+scope = 'playlist-modify-private'
+try:
+	username = str(1246074730) ## get from spotify app. ID : 1246074730
+	token = util.prompt_for_user_token(username, scope)
 except:
-	print("[!] Warning -- in try block 2 [!]")
+	os.remove(f".cache-{username}")
+	token = util.prompt_for_user_token(username, scope)
+sp = spotipy.Spotify(auth=token)
+playlist_id = '06YBjhkywYl6HEy7FMZaA9'
+success = []
+failed = []
+cnt = 1
+for song in song_list:
+	# q = encode("{}artist:{}".format(song['name'],song['artist']))
+	result = sp.search(q=song['name'],limit=1,offset=0,type='track')
+	if len(result['tracks']['items']) != 0:
+		results_list = result['tracks']['items'][0]
+		if results_list['album']['artists'][0]['name'] != song['artist'] and results_list['name'] != song['name']:
+			failed.append(song['name'])
+		else:
+			success.append(results_list['uri'])
+	else:
+		print('nope.')
 
-
-time_end = t()
+	cnt +=1
+	# del result	
+sp.user_playlist_add_tracks(username, playlist_id, success)
+t_e = t()
 print("end...")
-time_duration = round((time_end - time_start),2)
-print("script ran for {}s".format(time_duration))
+t_d = round((t_e - t_s),2)
+print("script ran for {}s".format(t_d))
 
 
